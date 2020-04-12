0ed39ce591db8679a4bbeb9175a1bd19c6fdd56a
Daniele Pia <>
2020-04-13 00:10:38 +0200

    Partial day 1

---
+++ Assets/DiceRoller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceRoller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DiceValues = new int[5];
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public int[] DiceValues;
+    
+
+    public void RollTheDice()
+    {
+
+    	for (int i = 0; i < 5; i++)
+    	{
+    		DiceValues[i] = Random.Range(1,7);
+    		Debug.Log("Die" + i + ": " + DiceValues[i]);
+
+    	}
+
+    }
+}
